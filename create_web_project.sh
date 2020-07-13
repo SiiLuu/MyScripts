b8f76a009f103a6d4365e7db0376842bf46707b0
lucas simao <siluMSI@linux.home>
2020-07-13 13:26:46 +0200

    [MODIF] create_web_project.sh

--- create_web_project.sh
+++ create_web_project.sh
@@ -14,7 +14,7 @@ cd ./src
 mkdir html
 cd ./html
 touch index.html
-echo "<!DOCTYPE html>°<head>°£<meta charset=\"utf-8\">°£<link rel=\"stylesheet\" href=\"../styles/main.css\">°£<title>...</title>°</head>°°<body>°°</body>°" >> index.html
+echo "<!DOCTYPE html>°°<html>°°£<!-- head -->°£<head>°££<meta charset=\"utf-8\">°££<title>BuBuche</title>°°££<!-- css -->°££<link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bulma@0.9.0/css/bulma.min.css\">°££<link rel=\"stylesheet\" href=\"../styles/main.css\">°££<!-- css -->°°££<!-- font -->°££<link href=\"https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap\" rel=\"stylesheet\">°££<script src=\"https://kit.fontawesome.com/ce04972de4.js\" crossorigin=\"anonymous\"></script>°££<!-- font -->°°££<!-- meta SEO -->°£££<meta name=\"description\" content=\"Ce site est un outil dont un bucheron dispose pour se faire connaître et vendre au mieux sont bois grâce à internet.\">°£££<meta name=\"keywords\" content=\"bois, chauffage, buche, livraison, domicile, arbre, maison\">°££<!-- meta SEO -->°°££<meta name=\"viewport\" content=\"width=device-width, user-scalable=no, initial-scale=1\">°£</head>°£<!-- head -->°°£<!-- body -->°£<body>°°££<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js\"></script>°££<script src=\"../js/main.js\"></script>°£</body>°£<!-- body -->°°</html>°" >> index.html
 cat index.html | sed -i "s/°/\n/g" index.html
 cat index.html | sed -i "s/£/\t/g" index.html
 cd ..
